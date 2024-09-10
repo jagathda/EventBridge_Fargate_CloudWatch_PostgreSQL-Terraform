253fd2ebc05d716571b16bb50c30ba698f6722bf
jagathda <mail.jagath@gmail.com>
2024-09-10 14:03:54 +0300

    Added security group

--- main.tf
+++ main.tf
@@ -24,3 +24,19 @@ resource "aws_subnet" "public_subnet_2" {
 resource "aws_internet_gateway" "fargate_igw" {
   vpc_id = aws_vpc.fargate_vpc.id
 }
+
+resource "aws_security_group" "fargate_sg" {
+  vpc_id = aws_vpc.fargate_vpc.id
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
