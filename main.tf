e07fcd161c96ef1733ef78c90dd93b5ac87288b8
jagathda <mail.jagath@gmail.com>
2024-09-10 13:58:22 +0300

    Added public subnets

--- main.tf
+++ main.tf
@@ -7,3 +7,16 @@ provider "aws" {
 resource "aws_vpc" "fargate_vpc" {
   cidr_block = "10.0.0.0/16"
 }
+
+#Public networks
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = aws_vpc.fargate_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-north-1a"
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id = aws_vpc.fargate_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-north-1b"
+}
